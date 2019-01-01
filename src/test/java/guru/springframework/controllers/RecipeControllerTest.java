8c0725e21be3dded06bc471cb338cb6abc19488e
Vadym Diachenko <vadim.dyachenko@gmail.com>
2019-01-01 20:48:21 +0200

    Added Recipe controller, tests and view page

---
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.service.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class RecipeControllerTest {
+
+    @Mock
+    private RecipeService recipeService;
+    private RecipeController controller;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+        Recipe recipe = Recipe.builder().id(1L).build();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(1L)).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/recipe/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipe"))
+                .andExpect(model().attribute("recipe", recipe));
+    }
+}
--- src/test/java/guru/springframework/service/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/service/RecipeServiceImplTest.java
@@ -8,9 +8,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -29,8 +33,32 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void getRecipes() {
+    public void getRecipeById() {
+        Long recipeId = 1L;
+        Recipe recipe = Recipe.builder().id(recipeId).build();
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
 
+        when(repository.findById(recipeId)).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = service.findById(recipeId);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(repository, times(1)).findById(recipeId);
+        verify(repository, never()).findAll();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getRecipeByIdShouldTrowRuntimeException() {
+        when(repository.findById(anyLong())).thenReturn(Optional.empty());
+
+        service.findById(1L);
+
+        verify(repository, times(1)).findById(1L);
+        verify(repository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipes() {
         Recipe recipe = new Recipe();
         Set<Recipe> recipeData = new HashSet<>();
         recipeData.add(recipe);

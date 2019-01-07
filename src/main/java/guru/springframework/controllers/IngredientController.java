e8dae1657c28dc28730bba6b76abbc3f6667a3eb
Vadym Diachenko <vadim.dyachenko@gmail.com>
2019-01-07 12:10:04 +0200

    Added ingredient controller and view page, added tests

---
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -0,0 +1,30 @@
+package guru.springframework.controllers;
+
+import guru.springframework.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    @Autowired
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{id}/ingredients")
+    public String listIngredients(@PathVariable Long id, Model model) {
+        log.debug("Getting list of ingredients for recipe id: {}", id);
+        model.addAttribute("recipe", recipeService.findCommandById(id));
+
+        return "recipe/ingredient/list";
+    }
+}
